9c02d57865ef5b02b111ffeaea5dd7bfb838e1eb
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-03-30 16:24:32 +0100

    Addition of a secret key to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k5rl7j**u8w=&zs3*_ux%rc^!i0krnnplvrbq*ee)czuca)g_*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
