5d32e92bd637b57da602fc0bb70b63736ad8468f
aaronalonsotorrens <aaronalonsotorrens@gmail.com>
2025-03-30 16:00:19 +0100

    Finalising database settings and deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k5rl7j**u8w=&zs3*_ux%rc^!i0krnnplvrbq*ee)czuca)g_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
